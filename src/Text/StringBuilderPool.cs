29413f8859af608657bb47a72ae99625586760b8
Kambiz Khojasteh <kambiz@delphiarea.com>
2019-08-24 11:08:31 +0200

    Introduce StringBuilderPool as a singleton object

---
+++ src/Text/StringBuilderPool.cs
@@ -0,0 +1,18 @@
+﻿using Assorted.Utils.Collections;
+using System.Text;
+
+namespace Assorted.Utils.Text
+{
+    /// <summary>
+    /// Defines a pool of reusable <see cref="StringBuilder"/> objects.
+    /// </summary>
+    /// <threadsafety static="true" instance="true"/>
+    public static class StringBuilderPool
+    {
+        /// <summary>
+        /// Gets the pool of <see cref="StringBuilder"/> objects.
+        /// </summary>
+        public static ObjectPool<StringBuilder> Instance { get; }
+            = new ObjectPool<StringBuilder>(2, () => new StringBuilder(), sb => sb.Length = 0);
+    }
+}
---
+++ tests/Text/StringBuilderPoolTests.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) 2019 Kambiz Khojasteh
+// This file is part of the Assorted.Utils package which is released under the MIT software license.
+// See the accompanying file LICENSE.txt or go to http://www.opensource.org/licenses/mit-license.php.
+
+using NUnit.Framework;
+using System.Text;
+
+namespace Assorted.Utils.Text.Tests
+{
+    [TestFixture]
+    [TestOf(typeof(StringBuilderPool))]
+    public class StringBuilderPoolTests
+    {
+        [Test]
+        public void TestImplicitCacting()
+        {
+            Assert.That(StringBuilderPool.Instance.Count, Is.Zero);
+            using (var proxy = StringBuilderPool.Instance.Acquire())
+            {
+                AppendSomething(proxy);
+                Assert.That(proxy.ToString(), Is.EqualTo("Something"));
+            }
+            Assert.That(StringBuilderPool.Instance.Count, Is.EqualTo(1));
+            StringBuilderPool.Instance.Clear();
+            Assert.That(StringBuilderPool.Instance.Count, Is.EqualTo(0));
+
+            void AppendSomething(StringBuilder sb) => sb.Append("Something");
+        }
+
+        [Test]
+        public void TestUpperBound()
+        {
+            Assert.That(StringBuilderPool.Instance.Count, Is.Zero);
+            Recurse(99);
+            Assert.That(StringBuilderPool.Instance.Count, Is.EqualTo(StringBuilderPool.Instance.MaxCapacity));
+            StringBuilderPool.Instance.Clear();
+            Assert.That(StringBuilderPool.Instance.Count, Is.EqualTo(0));
+
+            void Recurse(int count)
+            {
+                if (count <= 0)
+                    return;
+
+                using (var proxy = StringBuilderPool.Instance.Acquire())
+                {
+                    proxy.Value.Append($"Test {count}");
+                    Recurse(count - 1);
+                    Assert.That(proxy.ToString(), Is.EqualTo($"Test {count}"));
+                }
+                Assert.That(StringBuilderPool.Instance.Count, Is.LessThanOrEqualTo(StringBuilderPool.Instance.MaxCapacity));
+            }
+        }
+    }
+}
